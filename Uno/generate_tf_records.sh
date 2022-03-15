55ef07d7231bc5fe20ba05e5baad4d6279c24806
Daman Khaira <damank@graphcore.ai>
2022-03-14 18:29:28 -0700

    Adding variable to the script

--- Uno/generate_tf_records.sh
+++ Uno/generate_tf_records.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-#python uno_to_tfrecords.py --train_sources all --cache data_dir/cache --use_landmark_genes True --preprocess_rnaseq source_scale --no_feature_source True --no_response_source True -z 4096 --use_exported_data data_dir/all.h5 --export_data ALL_TFR --on_memory_loader False
-python uno_to_tfrecords.py --train_sources CCLE --cache data_dir/cache --use_landmark_genes True --preprocess_rnaseq source_scale --no_feature_source True --no_response_source True -z 4096 --use_exported_data data_dir/CCLE.h5 --export_data CCLE_TFR --on_memory_loader False
+SRC="CCLE"
+CACHE=./data_dir/cache # Change it to valid cache directory
+H5=./data_dir/CCLE.h5  # Change it to a valid H5 path
+BATCH=4096             # Number of samples stored in one tfrecord file
+TFR=$SRC_TFR	       # Directory containing TF Records
+
+python uno_to_tfrecords.py --train_sources $SRC --cache $CACHE --use_landmark_genes True --preprocess_rnaseq source_scale --no_feature_source True --no_response_source True -z $BATCH --use_exported_data $H5 --export_data $TFR --on_memory_loader False
